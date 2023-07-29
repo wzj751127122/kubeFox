cbc686db1917b4a463ee6ba13a71810e257ab9c9
allen <wuzijie@authing.cn>
2023-07-29 19:33:57 +0800

    添加了前端显示

--- main.go
+++ main.go
@@ -11,46 +11,12 @@ import (
 
 func main() {
 
-	// // 初始化配置文件
-	// err := config.Init("./setting.yaml")
-	// if err != nil {
-	// 	fmt.Printf("settings.Init() err:%v\n", err)
-	// 	return
-	// }
 
-	// fmt.Println("mode=", config.Conf.Mode)
-
-	// err = dao.Init(config.Conf.MysqlConfig)
-	// if err != nil {
-	// 	fmt.Printf("初始化mysql err:%v\n", err)
-	// 	return
-	// }
-
-	// defer dao.Close()
 	gin.SetMode(gin.ReleaseMode)
 	cmd := app.NewServerCommand()
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 
-	// r,err := utils.GenSaltPassword("zlovew1015")
-	// if err != nil {
-	// 	return
-	// }
-	// fmt.Println(r)
 
-	// d,_:=dao.Workflow.GetWorkflow("","blog",10,1)
-	// fmt.Println(d)
-
-	// 启动websocket
-	// go func() {
-	// 	http.HandleFunc("/ws", service.Terminal.WsHandler)
-	// 	http.ListenAndServe(":9999", nil)
-	// }()
-	//运行
-	// err = r.Run(fmt.Sprintf(":%d", viper.GetInt("app.port")))
-	// if err != nil {
-	// 	fmt.Printf("run server failed, err:%v\n", err)
-	// 	return
-	// }
 }
--- router/router.go
+++ router/router.go
@@ -9,14 +9,29 @@ import (
 	"k8s-platform/controller/user"
 	"k8s-platform/middle"
 
+	"github.com/gin-gonic/gin"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 var AlwaysAllowPath sets.String
 
 func InitRouter(opt *opention.Options) {
+
+	opt.GinEngine.Static("/static", "./static")
+	// opt.GinEngine.StaticFS("/", http.Dir("./static"))
+
+	opt.GinEngine.GET("/", func(ctx *gin.Context) {
+		// ctx.HTML(http.StatusOK, "index.html", nil)
+		ctx.File("static/index.html")
+	})
+	// 捕获所有路径，返回前端应用的 index.html
+	opt.GinEngine.NoRoute(func(c *gin.Context) {
+		c.File("static/index.html")
+	})
 	apiGroup := opt.GinEngine.Group("/api")
 	middle.InstallMiddlewares(apiGroup)
+	// opt.GinEngine.LoadHTMLFiles("./static/index.html")
+
 	//安装不需要操作记录路由
 	{
 		// api.NewApiRouter(apiGroup)
